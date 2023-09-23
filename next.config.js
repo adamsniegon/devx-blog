bd27d2262a070d423d9013998cadd305935ec3a9
Adam Sniegoň <adam1030@post.cz>
2023-09-23 21:16:59 +0200

    build: Initial setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    sassOptions: {
+        additionalData: `@import "app/_breakpoints.scss";`
+    }
+}
 
 module.exports = nextConfig
